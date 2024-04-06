8d1db821c35c128254104cb01caf768352fb832b
Lyutfi <lutfickl@hotmail.com>
2024-04-06 10:27:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/src'));
+const port = 3000;
+app.listen(port);
+console.log('Server runs on '+port);
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
