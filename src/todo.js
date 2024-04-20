774a2e874fafee42c34d041f33addfa233609853
Lyutfi <lutfickl@hotmail.com>
2024-04-20 16:23:39 +0200

    Search and categories

--- src/todo.js
+++ src/todo.js
@@ -5,13 +5,24 @@ const tasksDiv = document.getElementById('tasks');
 const stats = document.getElementById('stats');
 const statsTotal = document.getElementById('stats-total');
 const statsCompleted = document.getElementById('stats-completed');
+const searchInput = document.getElementById('search-task');
+const tasksTop = document.getElementById('tasks-top');
+const categoryDiv = document.getElementById('category');
+const categorySearch = document.getElementById('categorySearch');
+const addCategoryForm = document.getElementById('addCategory');
+const modal = document.getElementById('crud-modal');
 
 let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+let categories = localStorage.getItem('categories') ? JSON.parse(localStorage.getItem('categories')) : 
+[{'id':0,'name':'Urgent','color':'red'},{'id':1,'name':'Work','color':'blue'},{'id':2,'name':'Learning','color':'indigo'}]
+localStorage.setItem('categories', JSON.stringify(categories));
+
 
 const loadTasks = () => {
     tasksDiv.innerHTML = ""
     for(let i = 0;i < tasks.length; i++){
-        tasksDiv.innerHTML += taskSuccess(tasks[i].name,tasks[i].completed)
+        if(!tasks[i].deleted) 
+        tasksDiv.innerHTML += taskCreate(tasks[i].id,tasks[i].name,tasks[i].category,tasks[i].completed)
     }
     deleteListeners()
     updateListeners()
@@ -19,39 +30,62 @@ const loadTasks = () => {
     loadStats()
 }
 
-const taskSuccess = (task,completed = false) => {
-    return `<div class="task ${ completed ? "completed" : ""}">
-        <span class="truncate">
-            <input type="checkbox" class="update-checkbox mr-1 border-none bg-brand-50" ${ completed ? "checked" : ""}>
-            <input type="text" class="update-input min-w-80" value="${task}">
-         </span>
-        <div class="buttons">
-            <button class="update-button"><i class="fa-solid fa-pen"></i></button>
-            <button class="delete-button"><i class="fa-solid fa-trash"></i></button>
-        </div>
-</div>`
+const loadStats = () => {
+        let completed = 0
+        let totalStats = 0
+        for(const task in tasks){
+            if(!tasks[task].deleted) {
+                totalStats++
+                if(tasks[task].completed){
+                    completed++
+                }
+            }
+        }
+        statsTotal.innerText = totalStats
+        statsCompleted.innerText = completed
+        if(totalStats > 0){
+            stats.classList.contains('hidden') ? stats.classList.remove('hidden') : null;
+            tasksTop.classList.contains('hidden') ? tasksTop.classList.remove('hidden') : null;
+        } else {
+            stats.classList.add('hidden');
+            tasksTop.classList.add('hidden');
+        }
+   
 }
 
-const loadStats = () => {
-    if(tasks.length > 0){
-        let completed = 0
-        for(const task in tasks){
-            tasks[task].completed ? completed++ : null
-        }
-        statsTotal.innerText = tasks.length
-        statsCompleted.innerText = completed
-        stats.classList.contains('hidden') ? stats.classList.remove('hidden') : null
-    } else {
-        stats.classList.add('hidden')
-    }
+const loadCategories = () => {
+    categoryDiv.innerHTML = ''
+    categorySearch.innerHTML = ''
+    categories.forEach(cat => {
+        categoryDiv.innerHTML += `<option value="${cat.id}">${cat.name}</option>`;
+        categorySearch.innerHTML += `<div data-id="${cat.id}" class="category"><span class="h-2 w-2 bg-${cat.color}-600 mr-2 rounded-full"></span> <span class="truncate">${cat.name}</span></div>`
+    })
+    filterListeners()
+}
+
+const taskCreate = (id,task,category,completed = false) => {
+    return `<div class="task ${ completed ? "completed" : ""}">
+        <span class="truncate max-sm:text-clip">
+            <input type="checkbox" class="update-checkbox mr-1 w-4 h-4 accent-blue-600 bg-gray-100 bg-gray-300 rounded checked:bg-blue-800 checked:border-0" data-id="${id}" ${ completed ? "checked" : ""}>
+            <input type="text" class="update-input min-w-80" value="${task}">
+         </span>
+        <div class="buttons flex justify-center items-center gap-2">
+            <span class="h-5 w-5 bg-${categories[category].color}-600 mr-2 rounded-full inline-flex"></span>
+            <button class="update-button" data-id="${id}"><i class="fa-solid fa-pen"></i></button>
+            <button class="delete-button" data-id="${id}"><i class="fa-solid fa-trash"></i></button>
+        </div>
+</div>`
 }
 
 const addTask = (e) => {
     e.preventDefault();
     addTaskInput.value.trim() === '' ? vanillaToast.warning('Task can not be empty!', {duration:500, fadeDuration:500})
     : tasks.push({
+        id: tasks.length,
         name: addTaskInput.value.trim(),
-        completed: false
+        category: categoryDiv.value,
+        completed: false,
+        deleted: false
     })
     vanillaToast.success(`Task <b>${addTaskInput.value.trim()}</b> added successfully!`, {duration:500, fadeDuration:500})
     localStorage.setItem('tasks', JSON.stringify(tasks))
@@ -61,18 +95,43 @@ const addTask = (e) => {
 
 addTaskForm.addEventListener('submit', addTask)
 
+const addCategory = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const name = data.get('name');
+    const color = data.get('color');
+    name.trim() === '' ? vanillaToast.warning('Category name can not be empty!', {duration:500, fadeDuration:500})
+    : categories.push({
+        id: categories.length,
+        name: name.trim(),
+        color: color
+    })
+    vanillaToast.success(`Category <b>${name.trim()}</b> added successfully!`, {duration:500, fadeDuration:500})
+    localStorage.setItem('categories', JSON.stringify(categories))
+    addCategoryForm.reset()
+    loadCategories()
+}
+addCategoryForm.addEventListener('submit', addCategory)
+
+// Listeners
 const deleteListeners = () => {
     let deleteBtn = document.querySelectorAll(".delete-button")
-    deleteBtn.forEach((button, index) => {
-      button.addEventListener("click", () => { deleteTask(index) })
+    deleteBtn.forEach((button) => {
+      button.addEventListener("click", () => { deleteTask(button.dataset.id) })
     })
 }
 
 const updateListeners = () => {
     let updateBtn = document.querySelectorAll(".update-button")
     let updateInput = document.querySelectorAll(".update-input")
+    updateInput.forEach((input, index) => {
+        input.addEventListener("click", () => { 
+            updateBtn[index].innerHTML = '<i class="fa-solid fa-check"></i>' 
+            updateBtn[index].classList.add('active')
+        })
+      })
     updateBtn.forEach((button, index) => {
-      button.addEventListener("click", () => { updateTask(index,updateInput[index].value) })
+      button.addEventListener("click", () => { updateTask(button.dataset.id,updateInput[index].value) })
     })
 }
 
@@ -82,17 +141,35 @@ const completeListeners = () => {
     updateCheckbox.forEach((update, index) => {
         update.addEventListener("click", () => { 
         taskArea[index].classList.toggle('completed')
-        completeTask(index) })
+        completeTask(update.dataset.id) })
     })
   }
 
+const filterListeners = () => {
+    let filterButton = document.querySelectorAll(".category")
+    filterButton.forEach((button) => {
+        button.addEventListener("click", () => { 
+        if(button.classList.contains('active')){
+            loadTasks()
+            button.classList.toggle('active')
+        } else{
+            button.classList.toggle('active')
+            filterCategory(button.dataset.id) 
+        }
+        })
+    })
+}
+
+// -- Listeners
+
+// Update Functions
 const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 const deleteTask = (index) => {
-    vanillaToast.info(`Task <b>${tasks[index].name}</b> deleted successfully!`, {duration:500, fadeDuration:500})
-    tasks.splice(index,1)
+    vanillaToast.error(`Task <b>${tasks[index].name}</b> deleted successfully!`, {duration:500, fadeDuration:500})
+    tasks[index].deleted = true;
     updateLocal()
     loadTasks()
 }
@@ -110,7 +187,43 @@ const completeTask = (index) => {
     updateLocal()
     loadTasks()
 }
+// --Update Functions
+
+const searchTask = (value) => {
+   if (value.trim() === '') {
+            loadTasks()
+        } else {
+            tasksDiv.innerHTML = ""
+            for(let i = 0;i < tasks.length; i++){
+                if(tasks[i].name.includes(value)){
+                    if(!tasks[i].deleted) 
+                    tasksDiv.innerHTML += taskCreate(tasks[i].id,tasks[i].name,tasks[i].category,tasks[i].completed)
+                }
+            }
+            deleteListeners()
+            updateListeners()
+            completeListeners()
+            loadStats()
+        }
+    }
+searchInput.addEventListener('keyup', () => searchTask(searchInput.value))
+
+const filterCategory = (value) => {
+    tasksDiv.innerHTML = ""
+    for(let i = 0;i < tasks.length; i++){
+        if(tasks[i].category == value){
+            if(!tasks[i].deleted) 
+            tasksDiv.innerHTML += taskCreate(tasks[i].id,tasks[i].name,tasks[i].category,tasks[i].completed)
+        }
+    }
+    deleteListeners()
+    updateListeners()
+    completeListeners()
+    loadStats()
+}
+
 
 window.onload = () => {
     loadTasks()
+    loadCategories()
 }
